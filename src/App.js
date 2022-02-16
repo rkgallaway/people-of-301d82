6bbdd810d6de00dff7591ebb34156342b6ee7a59
Ryan Gallaway <rkgallaway@msn.com>
2022-02-16 10:29:48 -0800

    demo 2 complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js'
-import Main from './Main.js'
+import Header from './Header.js';
+import Main from './Main.js';
+import data from './data.json';
+
 
 class App extends React.Component {
   render() {
     return (
       <>
       <Header />
-        <Main />
+        <Main data={data}/>
         <footer>
           &copy;CodeFellows 2022
         </footer>
