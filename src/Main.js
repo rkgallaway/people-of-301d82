7f80a3926fdf050d7677c13cee8d5dc3ad654a16
Ryan Gallaway <rkgallaway@msn.com>
2022-02-16 11:58:01 -0800

    lab-03 demo complete

--- src/Main.js
+++ src/Main.js
@@ -11,9 +11,7 @@ class Main extends React.Component {
     // proof that data is being passed successfully from App.js
     console.log(this.props)
 
-    let people = [];
-    this.props.data.forEach((person, index) => {
-      people.push(
+    let people = this.props.data.map((person, index) => (
         <Person
           key={index}
           name={person.name}
@@ -21,8 +19,8 @@ class Main extends React.Component {
           addHearts={this.props.addHearts}
           handleShowModal={this.props.handleShowModal}
         />
-      )
-    })
+    ));
+
     return (
       <main>
         <Row xs={1} sm={2} md={3} lg={4}>
